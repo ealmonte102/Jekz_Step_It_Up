a80d5270fdc4eb8e913287fbe9901cee319691b4
evanalmonte <evan.almonte@gmail.com>
2017-12-08 19:08:18 -0500

    Change the way item interactor handles items
    - Parse only 1 array of items
    - items contain an extra value denoting their type.

--- app/src/main/java/com/jekz/stepitup/model/item/ItemInteractor.java
+++ app/src/main/java/com/jekz/stepitup/model/item/ItemInteractor.java
@@ -3,6 +3,7 @@ package com.jekz.stepitup.model.item;
 import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.util.Log;
 import android.util.Pair;
 
 import com.jekz.stepitup.R;
@@ -36,10 +37,7 @@ public class ItemInteractor {
         HashMap<String, Integer> modelList = extractModels(resources);
         LinkedHashMap<Integer, Pair<Item, Integer>> itemList = new LinkedHashMap<>();
         populateMiscItems(itemList, Item.Item_Type.MISC, resources, R.array.misc);
-        populateItemList(itemList, Item.Item_Type.HAT, resources, R.array.hats);
-        populateItemList(itemList, Item.Item_Type.SHIRT, resources, R.array.shirts);
-        populateItemList(itemList, Item.Item_Type.PANTS, resources, R.array.pants);
-        populateItemList(itemList, Item.Item_Type.SHOES, resources, R.array.shoes);
+        populateItemList(itemList, resources, R.array.items);
         instance = new ItemInteractor(itemList, modelList);
         return instance;
     }
@@ -63,20 +61,35 @@ public class ItemInteractor {
     }
 
     @SuppressLint("ResourceType")
-    private static void populateItemList(HashMap<Integer, Pair<Item, Integer>> itemList, Item
-            .Item_Type type, Resources res, int arrayID) {
+    private static void populateItemList(HashMap<Integer, Pair<Item, Integer>> itemList,
+                                         Resources res, int arrayID) {
         int numOfAttributes = res.getInteger(R.integer.num_item_attributes);
         TypedArray typedArray = res.obtainTypedArray(arrayID);
         int arrayLength = typedArray.length();
         for (int i = 0; i < arrayLength; i += numOfAttributes) {
-            Item item = new Item(
-                    typedArray.getString(i + 1),
-                    type,
-                    typedArray.getBoolean(i + 2, false),
-                    typedArray.getInteger(i + 3, -1));
+            String name = typedArray.getString(i + 1);
+            Log.d("Name", name);
+            boolean isAnimate = typedArray.getBoolean(i + 3, false);
+            int price = typedArray.getInteger(i + 4, -1);
+            Item.Item_Type type = getTypeFromString(typedArray.getString(i + 2));
+            Item item = new Item(name, type, isAnimate, price);
             itemList.put(item.getId(), new Pair<>(item, typedArray.getResourceId(i, 0)));
         }
         typedArray.recycle();
+    }
+
+    private static Item.Item_Type getTypeFromString(String string) {
+        switch (string.toLowerCase()) {
+            case "hat":
+                return Item.Item_Type.HAT;
+            case "shirt":
+                return Item.Item_Type.SHIRT;
+            case "pants":
+                return Item.Item_Type.PANTS;
+            case "shoes":
+                return Item.Item_Type.SHOES;
+        }
+        return null;
     }
 
     private static HashMap<String, Integer> extractModels(Resources res) {
