6f44a800f598dd3a3596e972b101e86d5fbe7cd3
evanalmonte <ealmonte102@qc.cuny.edu>
2017-12-15 21:19:29 -0500

    Add AsyncResponse interface to return JSONObject from requests

---
+++ app/src/main/java/com/jekz/stepitup/ui/shop/AsyncResponse.java
@@ -0,0 +1,11 @@
+package com.jekz.stepitup.ui.shop;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Owner on 12/3/2017.
+ */
+
+public interface AsyncResponse {
+    void processFinish(JSONObject output);
+}
